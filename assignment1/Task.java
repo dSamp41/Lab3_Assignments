fce45b2a6a8602564ef6d006bcec122294ac0fee
dSamp41 <d.sampietro@studenti.unipi.it>
2023-09-25 12:09:59 +0200

    Imporved prime number calculator

--- assignment1/Task.java
+++ assignment1/Task.java
@@ -1,9 +1,11 @@
 package Assignments.Lab3_Assignments.assignment1;
 
+import java.util.ArrayList;
+
 public class Task implements Runnable {
-    private final int MAX = 10000000;
+    private final int MAX = 1_000_000;
     
-    private boolean isPrime(int n){
+    /*private boolean isPrime(int n){
         boolean flag = false;
         for (int i = 2; i <= n / 2; ++i) {
             // condition for nonprime number
@@ -14,18 +16,36 @@ public class Task implements Runnable {
         }
 
         return !flag;
+    }*/
+
+    private void getPrimes(){
+        ArrayList<Integer> primes = new ArrayList<Integer>();
+        primes.add(2);
+        primes.add(3);
+        
+        for(int i=4; i<=MAX; i++){
+            boolean isPrime = true;
+            for (Integer p : primes){
+                if(i % p == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                primes.add(i);
+            }
+        }
+        
+        System.out.printf("Num of primes: %d\n", primes.size());
+
     }
 
     public void run(){
-        int primes = 0;
-        for(int i=0; i<MAX; i++){
-            if(isPrime(i)){ 
-                primes++;
-            }
-        }
+        long startTime = System.nanoTime();
+        getPrimes();
+        long time = System.nanoTime() - startTime;
 
-        System.out.printf("Num of primes: %d\n", primes);
-
+        long timeMilli = time / (1_000_000);
+        System.out.println("Thread execution time: " + timeMilli + " ms");
     }
-    
 }
