624f4460ca24b0408d238c6e62202ff756dda18a
dSamp41 <d.sampietro@studenti.unipi.it>
2023-09-26 15:09:53 +0200

    Implement thread pooling

--- assignment1/Main.java
+++ assignment1/Main.java
@@ -1,14 +1,42 @@
 package Assignments.Lab3_Assignments.assignment1;
+/*
+Scrivere una applicazione JAVA che:
+    - crea e attiva n thread
+    - ogni thread esegue esattamente lo stesso task, ovvero conta il numero di interi minori di 10,000,000 che sono primi
+    - il numero di thread che devono essere attivati e mandati in esecuzione viene richiesto all’utente, che lo inserisce tramite la CLI (Command Line Interface)
+
+Analizzare come varia il tempo di esecuzione dei thread attivati a seconda del loro numero
+Sviluppare quindi un programma in cui si creano n task, tutti eseguono la computazione descritta in precedenza e vengono sottomessi a un threadpool la cui dimensione deve essere inserita da CLI.
+*/
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args){
+        System.out.println("Dimensione pool: ");
+        int size = Integer.parseInt(System.console().readLine());
+
+        System.out.println("Numero di thread: ");
+        int numThreads = Integer.parseInt(System.console().readLine());
+
+
+        ExecutorService taskExecutor = Executors.newFixedThreadPool(size);
+        for(int i=0; i<=numThreads; i++){
+            taskExecutor.execute(new Task());
+        }
+        taskExecutor.shutdown();
+
+
+        /*
+        System.out.println("Numero di thread: ");
         int numThreads = Integer.parseInt(System.console().readLine());
 
         for(int i=0; i<numThreads; i++){
             Task task = new Task();
             Thread thread = new Thread(task);
             thread.start();
-        }
+        }*/
     
     }
 }
--- assignment1/Task.java
+++ assignment1/Task.java
@@ -4,19 +4,7 @@ import java.util.ArrayList;
 
 public class Task implements Runnable {
     private final int MAX = 1_000_000;
-    
-    /*private boolean isPrime(int n){
-        boolean flag = false;
-        for (int i = 2; i <= n / 2; ++i) {
-            // condition for nonprime number
-            if (n % i == 0) {
-                flag = true;
-                break;
-            }
-        }
 
-        return !flag;
-    }*/
 
     private void getPrimes(){
         ArrayList<Integer> primes = new ArrayList<Integer>();
