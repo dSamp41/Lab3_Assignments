57e29c0f10119b3c941b28470943f4bec70aaab7
dSamp41 <d.sampietro@studenti.unipi.it>
2023-10-11 18:32:17 +0200

    First sketch for Lab
    Test userComparator and priority queue

--- assignment3/Lab.java
+++ assignment3/Lab.java
@@ -1,3 +1,20 @@
+import java.util.concurrent.PriorityBlockingQueue;
+
 public class Lab {
+    public static void main(String[] args){
+        PriorityBlockingQueue<User> prQueue = new PriorityBlockingQueue<>(10, new UserComparator());
+        
+        prQueue.add(new Student());
+        prQueue.add(new Professor());
+        prQueue.add(new Thesist(5));
+        prQueue.add(new Professor());
+
+        System.out.println("Users in queue:");
+        while (!prQueue.isEmpty()) {
+            System.out.println(prQueue.poll());
+        }
+    
+    }
+
     
 }
