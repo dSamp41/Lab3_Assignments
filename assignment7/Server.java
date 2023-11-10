4f2e8a64925e27e95e99a0405dc877a6f445ca6e
dSamp41 <d.sampietro@studenti.unipi.it>
2023-11-10 10:47:03 +0100

    Extract Game task to new class

--- assignment7/Server.java
+++ assignment7/Server.java
@@ -1,8 +1,5 @@
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -21,76 +18,6 @@ public class Server{
         }
         catch(IOException e){
             System.err.println(e.getMessage());
-        }
-    }
-
-    //Simulazione game
-    private static class Game implements Runnable{
-        Socket clientSocket;
-        GameInstance game = new GameInstance();
-
-        public Game(Socket s){
-            this.clientSocket = s;
-        }
-
-        public void run(){
-            System.out.println("New player: " + clientSocket);
-            
-            try(Scanner in = new Scanner(clientSocket.getInputStream());
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)){
-                
-                out.println("Game:\t\t" + game.toString());
-
-                while(in.hasNextLine()){
-                    //command processing 
-                    processCommand(in, out);
-                }
-            }
-            catch(IOException e){
-                System.err.println(e.getMessage());
-            }
-            finally{
-                System.out.println("A player left: " + clientSocket);
-            }
-        }
-
-        private void processCommand(Scanner in, PrintWriter out) {
-            switch (in.nextLine()) {
-                case "1":
-                    game.fight();
-                    out.println("Fighting:\t" + game.toString());
-                    break;
-                case "2":
-                    game.usePotion();
-                    out.println("Drinking:\t"  + game.toString());
-                    break;
-                case "3":
-                    //out.println("Quitting...");
-                    try {
-                        clientSocket.close();
-                    } catch (IOException e) {
-                        System.out.println(e.getMessage());
-                    }
-                    break;
-
-                case "y":
-                    game.newGame();
-                    out.println("New game:\t"  + game.toString());
-                    break;
-
-                case "n":
-                    out.println("See you again!");
-                    try {
-                        clientSocket.close();
-                    } catch (IOException e) {
-                        System.out.println(e.getMessage());
-                    }
-                    break;
-
-                default:
-                    out.println("Invalid command");
-                    break;
-            }
         }
     }
 }
