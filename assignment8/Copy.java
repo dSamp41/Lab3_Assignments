0a9541d30dd5540fe6a11252f70664016c248a51
dSamp41 <d.sampietro@studenti.unipi.it>
2023-11-15 17:23:28 +0100

    Complete all implementations

--- assignment8/Copy.java
+++ assignment8/Copy.java
@@ -1,4 +1,6 @@
-import java.io.File;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -15,64 +17,143 @@ public class Copy{
             return;
         }
 
-        File f = new File(args[0]);
-        String[] parts = f.getName().split("\\.");
-        String outName = "out." + parts[parts.length - 1];
-        
-        try(ReadableByteChannel source = Channels.newChannel(new FileInputStream(f));
-            WritableByteChannel dest = Channels.newChannel(new FileOutputStream(outName))
+        String inpName = args[0];
+        String[] parts = inpName.split("\\.");
+        String extension = parts[parts.length - 1];
+
+        String outD = "outD." + extension;
+        String outI = "outI." + extension;
+        String outT = "outT." + extension;
+        String outB = "outB." + extension;
+        String outE = "outE." + extension;
+
+        int bufSize = 1024;
+
+        directBufferCopy(inpName, outD, bufSize);
+        indirectBufferCopy(inpName, outI, bufSize);
+        transferCopy(inpName, outT, bufSize);
+        bufferedStreamCopy(inpName, outB, bufSize);
+        explicitByteArray(inpName, outE, bufSize);
+    }
+
+    private static void directBufferCopy(String inpName, String outName, int bufSize){
+
+        try(ReadableByteChannel src = Channels.newChannel(new FileInputStream(inpName));
+            WritableByteChannel dest = Channels.newChannel(new FileOutputStream(outName));
         ){
-            //directBufferCopy(source, dest);
-            //indirectBufferCopy(source, dest);
-            //transferCopy(args[0], outName);
+            long start = System.currentTimeMillis();
+            ByteBuffer buffer = ByteBuffer.allocateDirect(bufSize);
+
+            while(src.read(buffer) != -1){
+                buffer.flip();
+                dest.write(buffer);
+                buffer.compact();
+            }
+            buffer.flip();
+            while(buffer.hasRemaining()){dest.write(buffer);}
+            
+            long end = System.currentTimeMillis();
+            long delta = end - start;
+            System.out.println("Direct Buffer: " + delta + " ms");
         }
         catch(IOException e){
-            System.err.println(e.getMessage());
+            System.err.println("Error during direct buffer copy");
         }
     }
 
-    private static void directBufferCopy(ReadableByteChannel src, WritableByteChannel dest) throws IOException{
-        ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
-
-        while(src.read(buffer) != -1){
-            buffer.flip();
-            dest.write(buffer);
-            buffer.compact();
-        }
-        buffer.flip();
-        while(buffer.hasRemaining()){dest.write(buffer);}
-    }
-
-    private static void indirectBufferCopy(ReadableByteChannel src, WritableByteChannel dest) throws IOException{
-        ByteBuffer buffer = ByteBuffer.allocate(1024);
-
-        while(src.read(buffer) != -1){
-            buffer.flip();
-            dest.write(buffer);
-            buffer.compact();
-        }
-        buffer.flip();
-        while(buffer.hasRemaining()){dest.write(buffer);}
-    }
-
-    private static void transferCopy(String inpPath, String outPath){
-        try(FileInputStream fromFile = new FileInputStream(inpPath);
-            FileOutputStream toFile = new FileOutputStream(outPath);
+    private static void indirectBufferCopy(String inpName, String outName, int bufSize){
+        try(ReadableByteChannel src = Channels.newChannel(new FileInputStream(inpName));
+            WritableByteChannel dest = Channels.newChannel(new FileOutputStream(outName));
         ){
+            long start = System.currentTimeMillis();
+            ByteBuffer buffer = ByteBuffer.allocate(bufSize);
+
+            while(src.read(buffer) != -1){
+                buffer.flip();
+                dest.write(buffer);
+                buffer.compact();
+            }
+            buffer.flip();
+            while(buffer.hasRemaining()){dest.write(buffer);}
+            
+            long end = System.currentTimeMillis();
+            long delta = end - start;
+
+            System.out.println("Indirect Buffer: " + delta + " ms");
+        }
+        catch(IOException e){
+            System.err.println("Error during indirect buffer copy");
+        }
+    }
+
+    private static void transferCopy(String inpName, String outName, int bufSize){
+        try(FileInputStream fromFile = new FileInputStream(inpName);
+            FileOutputStream toFile = new FileOutputStream(outName);
+        ){
+            long start = System.currentTimeMillis();
+
             FileChannel fromChannel = fromFile.getChannel();
             FileChannel toChannel = toFile.getChannel();
             
             long position = 0;
             long count = fromChannel.size();
             toChannel.transferFrom(fromChannel, position, count);
+
+            long end = System.currentTimeMillis();
+            long delta = end - start;
+            System.out.println("Transfer: " + delta + " ms");
         } 
         catch (Exception e) {
             System.err.println(e.getMessage());
         }
     }
 
-    //TODO
-    private static void bufferedStreamCopy(ReadableByteChannel src, WritableByteChannel dest) throws IOException{
+    private static void bufferedStreamCopy(String inpName, String outName, int bufSize){
+        try(BufferedInputStream inp = new BufferedInputStream(new FileInputStream(inpName));
+            BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(outName));
+        ){
+            long start = System.currentTimeMillis();
+
+            byte[] buffer = new byte[bufSize];
+            int bytesRead;
+
+            while((bytesRead = inp.read(buffer)) != -1){
+                out.write(buffer, 0, bytesRead);
+            }
+
+            long end = System.currentTimeMillis();
+            long delta = end - start;
+            System.out.println("Buffered Stream: " + delta + " ms");
+        }
+        catch(IOException e){
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private static void explicitByteArray(String inpName, String outName, int bufSize){
+        try(FileInputStream inp = new FileInputStream(inpName);
+            FileOutputStream out = new FileOutputStream(outName)
+        ){
+            long start = System.currentTimeMillis();
+
+            ByteArrayOutputStream baOut = new ByteArrayOutputStream();
+            byte[] buffer = new byte[bufSize];
+            int bytesRead;
+
+            while((bytesRead = inp.read(buffer)) != -1){
+                baOut.write(buffer, 0, bytesRead);
+            }
+
+            out.write(baOut.toByteArray());
+            baOut.close();
+
+            long end = System.currentTimeMillis();
+            long delta = end - start;
+            System.out.println("Byte Array: " + delta + " ms");
         
+        }
+        catch(IOException e){
+            System.err.println(e.getMessage());
+        }
     }
 }
